98d1cf30eb6cad7f4b0b62a250969812a38e2dc7
Davi Liporace <davimarquesr@gmail.com>
2022-11-13 14:32:04 -0300

    mongodb funcionando

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
-
-const app=express();
-
+const app = express();
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+try {
+  await mongoClient.connect();
+  console.log("MongoDB Conectado! ");
+} catch (err) {
+  console.log(err);
+}
 
+let db = mongoClient.db("UOL");
 
-app.listen(5000, () => {
-    console.log(`Server running in port: ${5000}`);
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`Server running in port: ${process.env.PORT}`);
+});
